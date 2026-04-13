54a17322b65bfde778e56fd01839a0fa79b1312e
F5BDE6 <f5bde6@proton.me>
2026-04-14 00:48:36 +0100

    Xeno fumo texture update
    and more :3

--- src/main/java/net/hellomouse/alexscavesenriched/forge/PlayerDeathEventHandler.java
+++ src/main/java/net/hellomouse/alexscavesenriched/forge/PlayerDeathEventHandler.java
@@ -1,12 +1,18 @@
 package net.hellomouse.alexscavesenriched.forge;
 
+import com.github.alexmodguy.alexscaves.server.item.ACItemRegistry;
+import net.hellomouse.alexscavesenriched.ACEBlockRegistry;
+import net.hellomouse.alexscavesenriched.ACEItemRegistry;
 import net.hellomouse.alexscavesenriched.AlexsCavesEnriched;
 import net.hellomouse.alexscavesenriched.item.DeadmanSwitchItem;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import org.jetbrains.annotations.Nullable;
 
 @Mod.EventBusSubscriber(modid = AlexsCavesEnriched.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class PlayerDeathEventHandler {
@@ -16,6 +22,24 @@ public class PlayerDeathEventHandler {
         for (var item : player.getInventory().items)
             if (item.getItem() instanceof DeadmanSwitchItem && (DeadmanSwitchItem.isActive(item)))
                 DeadmanSwitchItem.detonate(player.level(), player, item);
+            else if (event.getSource().getDirectEntity() instanceof LivingEntity livingEntity) {
+                var mainHand = livingEntity.getMainHandItem();
+                var isACWeapon = mainHand.is(ACEItemRegistry.FLAMETHROWER.get()) || mainHand.is(ACEItemRegistry.RAYGUN.get()) || mainHand.is(ACItemRegistry.RAYGUN.get());
+                @Nullable Item fumo = null;
+                switch (player.getName().getString()) {
+                    case "Xenonomous" -> {
+                        fumo = ACEBlockRegistry.XENO_FUMO.get().asItem();
+                    }
+                    case "Bowserinator" -> {
+                        fumo = ACEBlockRegistry.XIAOYU_FUMO.get().asItem();
+                    }
+                }
+                if (fumo != null && !player.level().isClientSide) {
+                    var itemEntity = new ItemEntity(player.level(), player.getX(), player.getY(), player.getZ(), fumo.getDefaultInstance());
+                    itemEntity.setDefaultPickUpDelay();
+                    player.level().addFreshEntity(itemEntity);
+                }
+            }
         for (var item : player.getInventory().offhand)
             if (item.getItem() instanceof DeadmanSwitchItem && (DeadmanSwitchItem.isActive(item)))
                 DeadmanSwitchItem.detonate(player.level(), player, item);
